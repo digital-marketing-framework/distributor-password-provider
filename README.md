a70b1a6c581fb89b34efd3aec8f49de59bec6ad6
Meelis Karulin <karulin@mediatis.de>
2023-11-15 10:48:11 +0100

    [BUGFIX] Write data into context so it can be shared between routes

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Password Provider for Mediatis Digital Marketing Framework
 
 ## Introduction
-Data Provider for generated Passwords
+Data Provider for generating unique plain-text passwords on submission.
+Password strength and length are configurable.
+
+## Security considerations
+Passwords are not encrypted and are stored with the submit data in the storage,
+You may want to consider disabling the storage if you don't want anyone with access to the backend
+and the distributor jobs area to access the data.
--- src/DataProvider/PasswordDataProvider.php
+++ src/DataProvider/PasswordDataProvider.php
@@ -15,6 +15,8 @@ use DigitalMarketingFramework\Distributor\PasswordProvider\Service\PasswordGener
 
 class PasswordDataProvider extends DataProvider
 {
+    public const KEY_FIELDS = 'fields';
+
     public const KEY_FIELD = 'field';
 
     public const DEFAULT_FIELD = 'password';
@@ -29,6 +31,8 @@ class PasswordDataProvider extends DataProvider
 
     public const KEY_ALPHABETS = 'alphabets';
 
+    public const KEY_PASSWORDS_CONTEXT = 'distributorPasswordProvider';
+
     public function __construct(
         string $keyword,
         RegistryInterface $registry,
@@ -40,23 +44,40 @@ class PasswordDataProvider extends DataProvider
 
     protected function processContext(ContextInterface $context): void
     {
+        $passwords = [];
+        foreach ($this->getListConfig(static::KEY_FIELDS) as $field) {
+            if (empty($field['field'])) {
+                continue;
+            }
+
+            $passwords[$field['field']] = $this->passwordGenerator->generate(
+                $this->getConfig(static::KEY_MIN_LENGTH),
+                $this->getConfig(static::KEY_MAX_LENGTH),
+                $this->getListConfig(static::KEY_ALPHABETS)
+            );
+        }
+
+        $this->submission->getContext()[self::KEY_PASSWORDS_CONTEXT] = $passwords;
     }
 
     protected function process(): void
     {
-        $password = $this->passwordGenerator->generate(
-            $this->getConfig(static::KEY_MIN_LENGTH),
-            $this->getConfig(static::KEY_MAX_LENGTH),
-            $this->getListConfig(static::KEY_ALPHABETS)
-        );
-        $this->setField($this->getConfig(static::KEY_FIELD), $password);
+        $passwords = $this->submission->getContext()[self::KEY_PASSWORDS_CONTEXT];
+        foreach ($passwords as $field => $password) {
+            $this->setField($field, $password);
+        }
     }
 
     public static function getSchema(): SchemaInterface
     {
         /** @var ContainerSchema */
         $schema = parent::getSchema();
-        $schema->addProperty(static::KEY_FIELD, new StringSchema(static::DEFAULT_FIELD));
+
+        $fieldsSchema = new ContainerSchema();
+        $fieldsSchema->addProperty(static::KEY_FIELD, new StringSchema(static::DEFAULT_FIELD));
+        $fieldsSchema->getRenderingDefinition()->setLabel('Field');
+
+        $schema->addProperty(static::KEY_FIELDS, new ListSchema($fieldsSchema));
         $schema->addProperty(static::KEY_MIN_LENGTH, new IntegerSchema(static::DEFAULT_MIN_LENGTH));
         $schema->addProperty(static::KEY_MAX_LENGTH, new IntegerSchema(static::DEFAULT_MAX_LENGTH));
 
